736148e91470cb6c02303e3a1816dffe41cef136
Roberto Bandeira de Mello <roberto.bandeiram@gmail.com>
2023-06-27 13:57:14 -0400

    Added CLI argparser

--- meme.py
+++ meme.py
@@ -1,7 +1,8 @@
 import os
 import random
-
-# @TODO Import your Ingestor and MemeEngine classes
+from QuoteEngine import QuoteModel, Ingestor
+from MemeEngine import MemeEngine
+import argparse
 
 
 def generate_meme(path=None, body=None, author=None):
@@ -40,9 +41,10 @@ def generate_meme(path=None, body=None, author=None):
 
 
 if __name__ == "__main__":
-    # @TODO Use ArgumentParser to parse the following CLI arguments
-    # path - path to an image file
-    # body - quote body to add to the image
-    # author - quote author to add to the image
-    args = None
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--body', type=str)
+    parser.add_argument('--author', type=str)
+    parser.add_argument('--path', type=str)
+
+    args = parser.parse_args()
     print(generate_meme(args.path, args.body, args.author))
